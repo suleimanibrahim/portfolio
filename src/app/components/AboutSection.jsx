eb29bcb2053f57b2f7730a61622be1a9b8708f48
suleiman ibrahim inuwa <isuleimanibrahim@gmail.com>
2024-07-24 09:43:20 -0700

    feat:added one project

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -10,13 +10,20 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>Java</li>
+        <li>Python</li>
+        <li>Django</li>
         <li>SpringBoot</li>
         <li>PostgreSQL</li>
+        <li>Mysql</li>
         <li>Nexjs</li>
+        <li>Angular</li>
         <li>JavaScript</li>
         <li>React</li>
+        <li>Git/Github</li>
         <li>Docker</li>
         <li>Kubernates</li>
+        <li>Kafka</li>
+        <li>Redis</li>
         <li>Microservices</li>
         <li>CI/CD</li>
       </ul>
@@ -60,18 +67,41 @@ const AboutSection = () => {
         <Image src="/images/about-image.png" alt="" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg text-justify">
-          Experienced Full Stack Developer skilled in Java, React, Nexjs and Angular, with a focus on building scalable web applications. Proficient in front-end and back-end development, I deliver projects from concept to deployment, employing intuitive user interface design and robust server-side logic.
-          Key skills: Java, Microservices architecture, Python, Angular, Nexjs, HTML5, CSS3, JavaScript, RESTful APIs. I excel in problem-solving and collaborate effectively with cross-functional teams, ensuring high-quality code within project timelines.
-          <p>
+          <p className="text-base lg:text-lg">
+          Experienced Full Stack Developer and Startup Catalyst
+         I am a versatile and skilled Full Stack Developer specialising in Java, Python, React, and Angular, with a focus on building scalable web applications. 
+         I excel in both front-end and back-end development, delivering projects from concept to deployment with intuitive user interface design and robust server-side logic.
+
+       <p className="font-bold mt-4">What Sets Me Apart?</p> 
+       <ul className="list-disc mt-3">
+        <li>
+          <span className="font-bold">Versatility & Expertise:</span> Proficient in multiple programming languages and web frameworks, 
+          I excel at Full Stack Development, allowing me to create robust and seamless applications. 
+          My deep understanding of Java, Python, React, Nexjs, and more enables me to adapt swiftly to new technologies and frameworks.  
+        </li> 
+        <li><span className="font-bold">Startup Catalyst:</span> My extensive experience with early-stage startups and major organisations has given me the expertise to help businesses raise seed rounds.</li> 
+        I play a crucial role in driving growth and success by building impactful products.
+        <li><span className="font-bold">Commitment to Quality:</span> I believe great software is a result of a thoughtful process. I practice Test-Driven Development (TDD) and write organised, high-quality test code that is integral to the source code itself.</li>
+        </ul>
+       <p className="font-bold mt-3">Why Choose Me?</p> 
+        I am dedicated to delivering top-notch solutions tailored to your unique requirements. My expertise in Full Stack Development allows me to craft intelligent and efficient applications. 
+        By combining technical skills with a deep understanding of development best practices, I ensure a seamless collaboration for success.
+        Let work together to transform your ideas into reality. Contact me today, and let elevate your projects with intelligent solutions and impeccable software engineering.
+        <p className="font-bold mb-2">
           <br/>
-          Highlights:
+           Key Skills and Expertise:
           <br/>
-          </p>
-          Optimize performance and enhance user experience
-          Embrace continuous integration and test-driven development
-          Stay updated with industry trends and best practices
-          Passionate about delivering innovative solutions that exceed expectations. Open to new challenges and opportunities for growth.
+        </p>
+          ✅Web Frameworks: Spring Boot, Django
+          ✅Databases: PostgreSQL, MySQL, MongoDB, DynamoDB, Google Cloud Datastore, BigQuery
+          ✅APIs: Google Ads API, Facebook Business API
+          ✅DevOps: Kubernetes, Docker, AWS Services
+          ✅Message Queues: Kafka, RabbitMQ, ActiveMQ, Artemis, AWS SQS
+          ✅Authentication: Keycloak, Apache Camel
+          ✅Architecture: Microservices, API Gateway, Service Discovery (Kubernetes), Configuration Management
+          ✅Search Technologies: Elastic Search, Apache Solr
+          ✅UI Frameworks: Angular, ReactJS, NexJs
+          ✅Others: Spring, Spring Boot, Spring Cloud, Spring Security, Hibernate, JPA, JDBC, REST API, SOAP, gRPC, GraphQL, Apache POI
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
